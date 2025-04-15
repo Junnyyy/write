47378c0a5efa73ca0208b1db3b87173a1eca6b8a
Johnny <44646589+Junnyyy@users.noreply.github.com>
2025-04-14 23:00:49 -0500

    style: remove editor border

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 import "./globals.css";
+import "./prosemirror.css";
 
 const notoSans = Noto_Sans({
   variable: "--font-noto-sans",
