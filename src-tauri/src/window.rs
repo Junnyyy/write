a0c607a429a2fc41124fe83d4c8395a3dd9c775f
Johnny <44646589+Junnyyy@users.noreply.github.com>
2025-04-24 20:56:01 -0500

    style: match titlebar to background color

--- src-tauri/src/window.rs
+++ src-tauri/src/window.rs
@@ -2,7 +2,7 @@ use tauri::{App, TitleBarStyle, WebviewUrl, WebviewWindowBuilder};
 
 pub fn create_main_window(app: &mut App) -> tauri::Result<()> {
     let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
-        .title("")
+        .title("Write")
         .inner_size(1100.0, 700.0);
 
     #[cfg(target_os = "macos")]
@@ -17,7 +17,7 @@ pub fn create_main_window(app: &mut App) -> tauri::Result<()> {
 
         let ns_window = window.ns_window().unwrap() as id;
         unsafe {
-            let bg_color = NSColor::colorWithRed_green_blue_alpha_(nil, 1.0, 1.0, 1.0, 1.0);
+            let bg_color = NSColor::colorWithRed_green_blue_alpha_(nil, 0.98, 0.98, 0.98, 1.0);
             ns_window.setBackgroundColor_(bg_color);
         }
     }
